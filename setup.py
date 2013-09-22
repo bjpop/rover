9a7b9aa72008b5398fff3eaf3691900959cbd9b7
Bernie Pope <florbitous@gmail.com>
2013-09-22 17:43:52 +1000

    Missing commas

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     description=(
         'ROVER-PCR Variant Caller: read-pair overlap '
         'considerate variant-calling software for PCR-based '
-        'massively parallel sequencing datasets.')
+        'massively parallel sequencing datasets.'),
     long_description=(
         'The software accepts a tab-delimited file (TSV) '
         'listing the coordinates of the target-specific primers used '
@@ -34,7 +34,7 @@ setup(
         'of and percentage of read-pairs a variant must be observed in for '
         'a "call" to be made. ROVER-PCR Variant Caller also reports the '
         'depth of coverage across amplicons to facilitate the identification '
-        'of any regions that may require further screening.')
+        'of any regions that may require further screening.'),
     install_requires=[
         "pysam >= 0.7.5"
     ],
