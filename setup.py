d84fce90acd97886362fae00deeb15b16e6d3bc8
Bernie Pope <florbitous@gmail.com>
2013-12-11 12:22:07 +1100

    Bumped version number to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='rover',
-    version='1.0.0',
+    version='1.0.1',
     author='Bernie Pope',
     author_email='bjpope@unimelb.edu.au',
     packages=['rover'],
